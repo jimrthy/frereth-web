d167ba11b3ff79bdb9aa13d5dffc7f562497954f
James Gatannah <james.gatannah@gmail.com>
2015-05-25 22:05:41 -0500

    Just copy/paste a fundamental example
    Based on:
    https://www.snip2code.com/Snippet/29969/Three-js-demo-with-ClojureScript--Actual
    
    That was posted a little over a year ago, and this version errors out
    because (AFAICT) the CanvasRenderer is either obsolete or changed to
    something that shouldn't be created directly.
    
    But the basic code seems to be loading and working. So apparently I was
    misinterpreting something and being overly pessimistic in my previous checkin

--- src/cljs/frereth/core.cljs
+++ src/cljs/frereth/core.cljs
@@ -1,8 +1,8 @@
 (ns ^:figwheel-always frereth.core
     "TODO: Do something interesting here"
     (:require [schema.core :as s]
-              #_[cljsjs.three]
-              [cljsjs.gl-matrix]
+              [cljsjs.three]
+              #_[cljsjs.gl-matrix]
               #_[cljsjs.d3]))
 
 (enable-console-print!)
@@ -13,13 +13,41 @@
 
 (defonce app-state (atom {:text "Hello from cljsjs!"}))
 
-(let [v1 (.fromValues js/vec3 1 10 100)
-      v2 (.fromValues js/vec3 1 1 1)
-      v3 (.create js/vec3)]
-  (.add js/vec3 v3 v1 v2)
-  (println (.str js/vec3 v1)
-           "+" (.str js/vec3 v2)
-           "=" (.str js/vec3 v3)))
+(comment (let [v1 (.fromValues js/vec3 1 10 100)
+               v2 (.fromValues js/vec3 1 1 1)
+               v3 (.create js/vec3)]
+           (.add js/vec3 v3 v1 v2)
+           (println (.str js/vec3 v1)
+                    "+" (.str js/vec3 v2)
+                    "=" (.str js/vec3 v3))))
+
+(comment
+  ;; TODO: What's a good way to make this silly thing get included?
+  (println (.version js/THREE)))
+
+(let [THREE js/THREE
+      camera (THREE.PerspectiveCamera. 75 (/ (.-innerWidth js/window)
+                                             (.-innerHeight js/window)) 1 10000)
+      scene (THREE.Scene.)
+      geometry (THREE.CubeGeometry. 200 200 200)
+      obj (js/Object.)]
+  (set! (.-z (.-position camera)) 10000)
+  (set! (.-color obj) 0xff0000)
+  (set! (.-wireframe obj) true)
+  (let [material (THREE.MeshBasicMaterial. obj)
+        mesh (THREE.Mesh. geometry material)
+        renderer (THREE.CanvasRenderer.)]
+    (.add scene mesh)
+    (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
+    (.appendChild (.-body js/document) (.-domElement renderer))
+    (letfn [(render []
+              (set! (.-x (.-rotation mesh)) (+ (.-x (.-rotation mesh)) 0.01))
+              (set! (.-y (.-rotation mesh)) (+ (.-y (.-rotation mesh)) 0.02))
+              (.render renderer scene camera))
+            (animate []
+              (.requestAnimationFrame js/window animate)
+              (render))]
+      (animate))))
 
 (s/defn reflect :- {s/Keyword s/Any}
   "TODO: This doesn't belong here. But it's probably a better
