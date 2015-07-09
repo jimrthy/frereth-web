7d2c8b6275b10a842f00a81da947b56470df2af8
James Gatannah <james.gatannah@gmail.com>
2015-07-09 03:53:58 +0000

    Refactor System unit tests to component-dsl
    To match earlier functionality move

--- test/frereth_web/system_test.clj
+++ test/frereth_web/system_test.clj
@@ -1,21 +1,5 @@
 (ns frereth-web.system-test
   (:require [clojure.test :as test :refer (is testing)]
             [com.frereth.web.system :refer :all]
+            [component-dsl.system :as cpt-dsl]
             [schema.core :as s]))
-
-(test/deftest schema-extractors
-  []
-  (testing "Minimalist schema loading"
-    (let [descr {'one '[schema-a schema-b]
-                 'two 'schema-a}]
-      (require-schematic-namespaces! descr)
-      (is (= (load-var 'one 'schema-a)
-             {:a s/Int, :b s/Int}))
-      (testing "Schema description merge"
-        ;; The order in which these are returned really is
-        ;; just an implementation detail
-        ;; For now, this approach makes the test quite a bit simpler
-        (is (= (extract-schema descr)
-               [{:a s/Int, :b s/Int}
-                {:z s/Str, :y s/Keyword}
-                {:a s/Str :z s/Int}]))))))
--- test/one.clj
+++
@@ -1,8 +0,0 @@
-(ns one
-  "Basic schema to test loading them"
-  (:require [schema.core :as s]))
-
-(def schema-a {:a s/Int, :b s/Int})
-(def schema-b {:z s/Str, :y s/Keyword})
-
-
