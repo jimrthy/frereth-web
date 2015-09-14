769b2106ea8e81180c0e01cf35726b6a29ea6021
James Gatannah <james.gatannah@gmail.com>
2015-09-14 00:48:38 +0000

    Write the bottom part of the load function
    The rest of it should be OK, but I haven't had a chance to test anything
    except the basic "find and slurp a .js file" functionality at the REPL

--- src/clj/com/frereth/web/loader.clj
+++ src/clj/com/frereth/web/loader.clj
@@ -9,20 +9,38 @@ Note that specialized scripts (such as anything that doesn't really belong in co
 should come from whichever server told the client to request them.
 
 Defining 'what really belong[s] in core' is worth a lot of consideration"
-  (:require [ribol.core :refer [raise]]
-            [schema.core :as s]))
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]
+            [ribol.core :refer [raise]]
+            [schema.core :as s])
+  (:import [java.net URL]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Internal
 
+(s/defn find-namespace :- (s/maybe URL)
+  [module-name :- s/Str
+   extension-search-order :- [s/Str]]
+  (let [names (string/split module-name #"\.")
+        folder-names (butlast names)
+        file-name (last names)
+        path (str "public/js/compiled/" (string/join "/" folder-names) "/" file-name ".")]
+    (when-let [almost-result (seq
+                              (take 1
+                                    (filter identity
+                                            (map (fn [extension]
+                                                   (let [actual-name (str path extension)]
+                                                     (println "Searching for" actual-name)
+                                                     (io/resource actual-name)))
+                                                 extension-search-order))))]
+      (first almost-result))))
+
 (s/defn load-cljs-namespace :- (s/maybe s/Str)
   [module-name :- s/Str
    extension-search-order :- [s/Str]]
-  ;; At least for a dumb starter version, should be able to split
-  ;; the ns by \. into folders, and then load from
-  ;; resources/public/js/compiled.
-  ;; That's in the classpath, right?
-  (raise {:not-implemented "Get this written"}))
+  (when-let [url (find-namespace module-name extension-search-order)]
+    ;; Note that there really isn't any reason to assume this is a file
+    (slurp (io/file url))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
