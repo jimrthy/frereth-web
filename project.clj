a749020ecdd548cea05640d2c0af1493e3f6af4e
James Gatannah <james.gatannah@gmail.com>
2016-07-22 03:33:15 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
                  [com.cognitect/transit-clj "0.8.285" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  ;; TODO: How many of these exclusions are still needed?
                  ;; And do more get added w/ this version bump?
-                 [org.immutant/immutant "2.1.4" :exclusions [clj-tuple
+                 [org.immutant/immutant "2.1.5" :exclusions [clj-tuple
                                                              org.clojure/clojure
                                                              org.clojure/java.classpath
                                                              org.clojure/tools.reader
@@ -77,17 +77,17 @@
                  [cljsjs/three "0.0.76-0"]
                  #_[cljsjs/d3 "3.5.5-3"]
                  [cljsjs/gl-matrix "2.3.0-jenanwise-0"]
-                 [com.cognitect/transit-cljs "0.8.237"]
+                 [com.cognitect/transit-cljs "0.8.239"]
                  ;; Q: Is there any remaining use for this?
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
-                 [org.clojure/clojurescript "1.9.76" :exclusions [org.clojure/clojure
+                 [org.clojure/clojurescript "1.9.93" :exclusions [org.clojure/clojure
                                                                   org.clojure/tools.reader]]
                  ;; TODO: This needs to go away.
                  ;; It's up to the individual Apps.
                  [org.omcljs/om "0.9.0" :exclusions [org.clojure/clojure]]
                  ;; TODO: This needs to go away.
                  ;; It's up to the individual Apps.
-                 [sablono "0.7.2"]
+                 [sablono "0.7.3"]
                  ;; TODO: Switch to bidi
                  ;; (it's built around plain data rather than macros
                  [secretary "1.2.3" :exclusions [org.clojure/clojure
@@ -96,7 +96,7 @@
                  ;;; Generally Useful
                  ;; Really should inherit my clojure version from this.
                  [com.frereth/client "0.1.0-SNAPSHOT"]
-                 [com.taoensso/sente "1.8.1" :exclusions [com.taoensso/encore
+                 [com.taoensso/sente "1.9.0" :exclusions [com.taoensso/encore
                                                           com.taoensso/timbre
                                                           org.clojure/clojure
                                                           org.clojure/core.async
